ce92f4490e4da001d2e074d47201dca2d6bdf416
Evgeniy] <bogdanov.es@phystech.edu>
2022-04-06 12:03:14 +0300

    add 4.3

--- Scripts/4/4.3.py
+++ Scripts/4/4.3.py
@@ -2,14 +2,27 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 
+def d2b(x):
+    return [int(bin) for bin in (bin(x)[2:]).zfill(8)]
+
+
 dac = [10, 9, 11, 5, 6, 13, 19, 26][::-1]
 led = 21
+pwm = 14
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(dac, GPIO.OUT)
 GPIO.setup(led, GPIO.OUT)
+GPIO.setup(pwm, GPIO.OUT)
+p = GPIO.PWM(pwm, 1000)
+p.start(0)
 
-p = GPIO.PWM(12, 0.5)
-p.start(1)
-input('Press return to stop:')   # use raw_input for Python 2
-p.stop()
-GPIO.cleanup()
+try:
+    while 1:
+        k = int(input("Enter duty cycle: "))
+        p.ChangeDutyCycle(k)
+finally:
+    GPIO.output(dac, 0)
+    GPIO.output(led, 0)
+    p.stop()
+    GPIO.output(pwm, 0)
+    GPIO.cleanup()
